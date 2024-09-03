040f86e851ab0ca7470eebdc6eff037e681a3400
Zijad Doglod <zijaddoglod01@gmail.com>
2024-09-03 15:38:58 +0200

    Backend is ready for developing

---
+++ localhub-be/localhub-be/Core/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace localhub_be.Core;
+public sealed class DatabaseContext : DbContext {
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
--- localhub-be/localhub-be/Program.cs
+++ localhub-be/localhub-be/Program.cs
@@ -1,8 +1,81 @@
+using localhub_be.Core;
+using Microsoft.OpenApi.Models;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
+
+if (string.IsNullOrEmpty(connectionString)) 
+    throw new Exception("Connection string not found. Ensure the .env file is correctly configured and placed in the root directory.");
+
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string?> {
+    { "ConnectionStrings:Database", connectionString }
+});
+
+builder.Services.AddDbContext<DatabaseContext>(options => {
+    var databaseVersion = builder.Configuration["DatabaseVersion"];
+    options.UseMySql(connectionString, new MySqlServerVersion(databaseVersion));
+});
+
+var secret = Environment.GetEnvironmentVariable("Secret");
+
+if (string.IsNullOrEmpty(secret))
+    throw new Exception("Secret not found. Ensure the .env file is correctly configured and placed in the root directory.");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret!)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "AuthenticationPolicy",
+                  policy => {
+                      policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                  });
+    options.AddDefaultPolicy(
+                  policy => {
+                      policy.AllowAnyOrigin().WithHeaders("Authorization").AllowAnyMethod();
+                  });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "LocalHub_API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -10,6 +83,8 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
