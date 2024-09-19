01bfe39bcb476e8f2d794ad732ca2a83e29fae4d
Zijad Doglod <zijaddoglod01@gmail.com>
2024-09-19 19:38:40 +0200

    New model added.

---
+++ localhub-be/localhub-be/Models/DTOs/UserLikedPostOut.cs
@@ -0,0 +1,8 @@
+﻿namespace localhub_be.Models.DTOs;
+public sealed record UserLikedPostOut {
+    public Boolean? IsLiked { get; init; }
+
+    public UserLikedPostOut(bool? isLiked) {
+        IsLiked = isLiked;
+    }
+}
