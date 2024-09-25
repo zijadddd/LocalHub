50d1643f02b1e30b58922ed29d63226f43b3a488
Zijad Doglod <94227360+zijadddd@users.noreply.github.com>
2024-09-25 16:42:41 +0200

    Alert popup functionality has been finished.

--- localhub-fe/src/app/components/alert-popup/alert-popup.component.ts
+++ localhub-fe/src/app/components/alert-popup/alert-popup.component.ts
@@ -1,18 +1,39 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Alert } from '../../shared/models/alert.model';
+import { CommunicationService } from '../../shared/services/communication.service';
+import { WhichAction } from '../../shared/models/which-action.model';
 
 @Component({
   selector: 'app-alert-popup',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf],
   templateUrl: './alert-popup.component.html',
   styleUrl: './alert-popup.component.css',
 })
 export class AlertPopupComponent implements OnInit {
-  public isWarning: boolean = false;
+  public isShowed: boolean = false;
+  public alert: Alert;
 
-  constructor(private readonly ref: ChangeDetectorRef) {}
+  constructor(
+    private readonly ref: ChangeDetectorRef,
+    private readonly communicationService: CommunicationService
+  ) {}
+
   ngOnInit(): void {
+    this.communicationService.data$.subscribe((response) => {
+      if (response.action === WhichAction.SHOW_ALERT_POPUP) {
+        this.alert = response.data;
+        this.isShowed = true;
+        this.ref.detectChanges();
+
+        setTimeout(() => {
+          this.isShowed = false;
+          this.ref.detectChanges();
+        }, 3500);
+      }
+    });
+
     this.ref.detectChanges();
   }
 }
