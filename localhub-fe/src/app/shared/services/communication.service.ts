af6705477b250dc1c10d3a37988070f9131df716
Zijad Doglod <94227360+zijadddd@users.noreply.github.com>
2024-09-25 16:48:05 +0200

    Opening modal method and is modal confirmed method bas heen finished.

--- localhub-fe/src/app/shared/services/communication.service.ts
+++ localhub-fe/src/app/shared/services/communication.service.ts
@@ -9,18 +9,22 @@ export class CommunicationService {
   private actionSource = new Subject<WhichAction>();
   action$ = this.actionSource.asObservable();
 
-  private dataSource = new Subject<any>();
+  private dataSource = new Subject<{ action: string; data: any }>();
   data$ = this.dataSource.asObservable();
 
   updateNavbarProfilePhoto() {
     this.actionSource.next(WhichAction.UPDATE_NAVBAR_PROFILE_PHOTO);
   }
 
-  openModal(data: any) {
-    this.dataSource.next(data);
+  openModal(action: string, data: any) {
+    this.dataSource.next({ action: action, data });
   }
 
-  isModalConfirmed(data: boolean) {
-    this.dataSource.next(data);
+  isModalConfirmed(action: string, data: boolean) {
+    this.dataSource.next({ action: action, data });
+  }
+
+  showAlertPopup(action: string, data: any) {
+    this.dataSource.next({ action: action, data });
   }
 }
