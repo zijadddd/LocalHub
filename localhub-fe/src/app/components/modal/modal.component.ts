9e988823491f1e470563888f44f9624ba01f6be7
Zijad Doglod <94227360+zijadddd@users.noreply.github.com>
2024-10-08 19:26:32 +0200

    Input for addiotinal reason has been added.

--- localhub-fe/src/app/components/modal/modal.component.ts
+++ localhub-fe/src/app/components/modal/modal.component.ts
@@ -3,22 +3,37 @@ import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from '../../shared/services/communication.service';
 import { Modal } from '../../shared/models/modal.model';
 import { WhichAction } from '../../shared/models/which-action.model';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [NgIf, NgClass],
+  imports: [NgIf, NgClass, ReactiveFormsModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
 export class ModalComponent implements OnInit {
   public modal: Modal;
+  public postForm: FormGroup;
 
   constructor(private readonly communicationService: CommunicationService) {}
   ngOnInit(): void {
     this.communicationService.data$.subscribe((response) => {
       if (response.action === WhichAction.OPEN_MODAL)
         this.modal = response.data;
+    });
+
+    this.postForm = new FormGroup({
+      reason: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(255),
+      ]),
     });
   }
 
@@ -32,9 +47,11 @@ export class ModalComponent implements OnInit {
 
   confirm() {
     this.modal.isShowed = !this.modal.isShowed;
-    this.communicationService.isModalConfirmed(
-      WhichAction.IS_MODAL_CONFIRMED,
-      true
-    );
+    const reason = this.postForm.get('reason')?.value || undefined;
+    this.postForm.reset();
+    this.communicationService.isModalConfirmed(WhichAction.IS_MODAL_CONFIRMED, [
+      true,
+      reason,
+    ]);
   }
 }
