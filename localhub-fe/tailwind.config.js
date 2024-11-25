649b5bc4bd8a9d1a80fb005b8f544589a61c25b3
Zijad Doglod <94227360+zijadddd@users.noreply.github.com>
2024-11-25 18:00:13 +0100

    A custom color palette has been added to the Tailwind configuration file for extended styling options.

--- localhub-fe/tailwind.config.js
+++ localhub-fe/tailwind.config.js
@@ -3,7 +3,52 @@ module.exports = {
   darkMode: "selector",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: "#17BF63",
+          light: "#166F47",
+          background: {
+            100: "#15202B",
+            200: "#131d27",
+            300: "#111a22",
+            400: "#0f161e",
+            500: "#0d131a",
+            600: "#0b1016",
+            700: "#080d11",
+            800: "#060a0d",
+            900: "#020304",
+          },
+          divider: "#343434",
+          warning: "#FF0000",
+        },
+        text: {
+          primary: "#FFFFFD",
+          secondary: "#87BF50",
+          disabled: "#A3A3A3",
+        },
+        menu: {
+          icon: "#6B7B87",
+          selectedIcon: "#17BF63",
+          textColor: "#FFFFFD",
+          selectedText: "#17BF63",
+        },
+        label: {
+          badgeBackground: "#575757",
+          badgeText: "#8093a2",
+          badgeRed: "#669fd5",
+        },
+        card: {
+          background: "#192734",
+        },
+        toggles: {
+          paperToggleButtonChecked: "#87BF50",
+          paperToggleButtonCheckedBar: "#598330",
+          paperToggleButtonUncheckedButton: "#A3A3A3",
+          paperToggleButtonUncheckedBar: "#6F6F6F",
+        },
+      },
+    },
   },
   plugins: [],
 };
